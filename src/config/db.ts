ec989d45a972254cf19b6138f6ca2dc01d543124
TKSOHLT <kimisami31@gmail.com>
2025-12-05 16:34:16 -0600

    🐛Fix: Se corrigiò la conexiòn a la base de datos, ahora se usa SSL

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
-import { Sequelize } from 'sequelize-typescript'
-import dotenv from 'dotenv'
+import { Sequelize } from 'sequelize-typescript';
+import dotenv from 'dotenv';
 
-dotenv.config() // Con esto se manda a llamar a las variables de entorno
+dotenv.config(); // Con esto se manda a llamar a las variables de entorno
 // console.log(process.env) // Con esto se pueden ver las variables env disponibles
 
 //!A continuación se muestra una conexión solo con el url dado de https://dashboard.render.com/d/dpg-d1v66dur433s73fd4ipg-a/recovery, (link de external connection), esto es sin SSL
@@ -22,8 +22,14 @@ dotenv.config() // Con esto se manda a llamar a las variables de entorno
 
 //? Pero lo recomendado siempre será el menor codigo, por lo que vamos a forzar el ssl con ?ssl=true
 const db = new Sequelize(process.env.DATABASE_URL!, {
-    models: [__dirname + '/../models/**/*'],
-    logging: false //Con esto no evnia nada a la consola
-})
+  models: [__dirname + '/../models/**/*'],
+  logging: false, //Con esto no evnia nada a la consola
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 export default db;
